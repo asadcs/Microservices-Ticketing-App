5ca8bdfcae830cef29392c46ba400fe888ac7de6
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-12-29 12:17:34 +0500

    CICD testing 12:17

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Bootstraping...');
+  console.log('Bootstraping12:17');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
