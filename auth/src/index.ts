3f25bb2bce02adb777861c2bde6fcb3c42b476a0
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-12-29 14:49:00 +0500

    2:50

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Bootstraping 2:44');
+  console.log('Bootstraping 2:50');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
